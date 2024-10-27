81c49051158b1cc9988f9f5b966072258475e674
Leo McClellan <mcclellan2@usf.edu>
2024-10-26 23:26:03 -0400

    Final Commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Task 2: Add Event Listener for Product Solution
-// Instantiate all buttons and paragraph ids. 
+// Instantiate all buttons and paragraph ids 
 const productSelector = document.getElementById('product-selector');
 const quantityInput = document.getElementById('quantity');
 const totalPriceElement = document.getElementById('total-price');
@@ -9,21 +9,22 @@ const orderSummary = document.getElementById('order-summary');
 // Task 3: Calculate Total Price Dynamically
 // Calculate total price when product or quantity changes
 function updateTotalPrice() {
+    // Update quantity and price when user updates quantity and produce
     const productPrice = parseFloat(productSelector.value);
     const quantity = parseInt(quantityInput.value);
     const totalPrice = productPrice * quantity;
     totalPriceElement.textContent = totalPrice.toFixed(2); //use toFixed to get 2 decimal places
 };
+
 // Use event listeners to update the price when the product or quantity change
 productSelector.addEventListener('change', updateTotalPrice);
 quantityInput.addEventListener('input', updateTotalPrice);
-
 
 // Task 4: Handle order submission
 placeOrderButton.addEventListener('click', function() {
     const selectedProduct = productSelector.options[productSelector.selectedIndex].text;
     const quantity = quantityInput.value;
     const totalPrice = totalPriceElement.textContent;
-    // When they place an order by clicking the button, give them a confirmation paragraph
+    // When user places an order by clicking the button, give them a confirmation paragraph
     orderSummary.textContent = `You ordered ${quantity} of ${selectedProduct}. Total price: $${totalPrice}`;
 });
